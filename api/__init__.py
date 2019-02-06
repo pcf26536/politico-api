425b4fa2ea43b55e1333aa5939c13f3b1243d91b
wainainad60 <muffwaindan@gmail.com>
2019-02-06 14:51:27 +0300

    [starts #163692451] creating directory structure

---
+++ api/__init__.py
@@ -0,0 +1,23 @@
+"""creating app"""
+#import os
+from flask import Flask
+from instance.config import app_config
+from api.ver1.parties.endpoints import party_bp
+
+def create_app(config_name):
+    """ create flask app with specified configs """
+    app = Flask(__name__, instance_relative_config=True) # instantiate the app
+
+    # set configuration
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+
+    ver_1_url_prefix = '/api/v1'
+
+    #register parties blueprint
+    app.register_blueprint(party_bp, url_prefix=ver_1_url_prefix)
+
+    #register office blueprint
+    #app.register_blueprint(office_blueprint, url_prefix=url_prefix_version_1)
+
+    return app
---
+++ instance/config.py
@@ -0,0 +1,40 @@
+"""Configuration file"""
+import os
+
+
+class Config(object):
+    """Parent configuration class."""
+    DEBUG = False
+    SECRET = os.getenv('SECRET')
+    DATABASE_URI = os.getenv('DATABASE_URL')
+
+
+class DevelopmentConfig(Config):
+    """Configurations for Development."""
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    """Configurations for Testing"""
+    TESTING = True
+    DEBUG = True
+
+
+class StagingConfig(Config):
+    """Configurations for Staging."""
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    """Configurations for Production."""
+    DEBUG = False
+    TESTING = False
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'debug': DevelopmentConfig,
+    'testing': TestingConfig,
+    'staging': StagingConfig,
+    'production': ProductionConfig,
+}
