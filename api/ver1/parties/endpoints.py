2ef84b4fccfcca9bc1aeb3131f1e080f8694b4ec
wainainad60 <muffwaindan@gmail.com>
2019-02-06 23:53:28 +0300

    [finishes #163691595 #163691579] add and get all parties

--- api/ver1/parties/endpoints.py
+++ api/ver1/parties/endpoints.py
@@ -5,3 +5,29 @@ from api.strings import *
 from .strings import *
 
 party_bp = Blueprint('parties', __name__) # init the blueprint for parties module
+
+@party_bp.route('/parties', methods=[post_method, get_method])
+def add_or_get_all_ep():
+    if request.method == post_method:
+        """ create party endpoint """
+        data = request.get_json()
+        form_data = request.form
+        #return error(str(len(data)), 201)
+        if not data or not len(data):
+            if form_data:
+                data = form_data
+            else:
+                return error("No data was provided", status_400)
+        try:
+            name = data[name_key]
+            hq_address = data[hqAddKey]
+            logo_url = data[logoUrlKey]
+        except KeyError as e:
+            return error("{} field is required".format(e.args[0]), status_400)
+
+        party = Party(name=name, hqAddress=hq_address, logoUrl=logo_url)
+        return party.add_party()
+
+    elif request.method == get_method:
+        return Party().get_parties()
+        
