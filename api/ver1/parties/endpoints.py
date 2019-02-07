d376847bd22b41e9bed172d618e6ca2e625a20bb
wainainad60 <muffwaindan@gmail.com>
2019-02-07 15:25:01 +0300

    added validate id for parties and offices

--- api/ver1/parties/endpoints.py
+++ api/ver1/parties/endpoints.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request
 from api.ver1.utils import error, no_entry_resp, field_missing_resp, method_not_allowed
-from api.ver1.validators import validate_dict
-from api.strings import name_key, post_method, get_method, status_400, patch_method, delete_method
+from api.ver1.validators import validate_dict, validate_id
+from api.strings import name_key, post_method, get_method, status_400, patch_method, delete_method, ok_str
 from .strings import hqAddKey, logoUrlKey, party_key
 from api.ver1.parties.controllers import cParty
 
@@ -35,26 +35,27 @@ def add_or_get_all_ep():
     else:
         return method_not_allowed(request.method)
 
+
 @party_bp.route('/parties/<int:id>', methods=[delete_method, get_method])
-def get_or_delete_ep(id):
+def get_or_delete_ep(party_id):
     try:
-        
-        party = cParty(id=id)
-        if request.method == get_method:
-            return party.get_party()
-        elif request.method == delete_method:
-            return party.delete_party()
+        party = cParty(id=party_id)
+        if validate_id(party_key, party_id) == ok_str:
+            if request.method == get_method:
+                return party.get_party()
+            elif request.method == delete_method:
+                return party.delete_party()
     except Exception as e:
         return error(str(e), 500)
 
 
 @party_bp.route('/parties/<int:id>', methods=[patch_method])
-def edit_ep(id):
+def edit_ep(party_id):
     try:
-        data = request.get_json()
-        #data = request.form
-        new_name = data[name_key]
-        party = cParty(id=id, name=new_name)
-        return party.edit_party()
+        if validate_id(party_key, party_id) == ok_str:
+            data = request.get_json()
+            new_name = data[name_key]
+            party = cParty(id=party_id, name=new_name)
+            return party.edit_party()
     except Exception as e:
         return error(str(e), 500)
