9eb62e979e1b244b4b895d970b3d80ed66a0882d
wainainad60 <muffwaindan@gmail.com>
2019-02-13 15:47:44 +0300

    added vote test data

--- api/ver2/utils/vote_test_data.py
+++ api/ver2/utils/vote_test_data.py
@@ -1,9 +1,19 @@
 from api.ver1.ballot.models import votes
 from api.strings import id_key
 from api.ver1.offices.strings import office_key
+from api.ver1.ballot.strings import candidate_key, createdBy_key, createdOn_key
 
 correct_vote = votes[0]
 del correct_vote[id_key]
 
 office_does_not_exist_vote = correct_vote
-office_does_not_exist_vote[]
+office_does_not_exist_vote[office_key] = 100
+
+candidate_does_not_exist_vote = correct_vote
+candidate_does_not_exist_vote[candidate_key] = 1000
+
+voter_does_not_exist_vote = correct_vote
+voter_does_not_exist_vote[createdBy_key] = 10000
+
+invalid_date_vote = correct_vote
+invalid_date_vote[createdOn_key] = 'fake date'
