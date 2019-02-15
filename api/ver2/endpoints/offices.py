7ae51ba84df88de2686cb04d05f9b1173d2ecdac
wainainad60 <muffwaindan@gmail.com>
2019-02-15 04:03:28 +0300

    [start #163996349] added get all offices endpoints

---
+++ api/ver2/endpoints/offices.py
@@ -0,0 +1,48 @@
+from flask import request, Blueprint
+from api.ver1.utils import field_missing_resp, runtime_error_resp, \
+    not_found_resp, check_form_data, no_entry_resp, success
+from api.ver2.models.offices import Office
+from api.strings import name_key, post_method, get_method, type_key, ok_str
+from api.ver1.offices.strings import office_key
+from api.ver1.validators import validate_dict, validate_id
+
+office_v1 = Blueprint('offices_v1', __name__)
+
+
+@office_v1.route('/offices', methods=[post_method, get_method])
+def add_or_get_all_ep():
+    if request.method == post_method:
+        fields = [name_key, type_key]
+        data = check_form_data(office_key, request, fields)
+        if not data:
+            return no_entry_resp(office_key, fields)
+        try:
+            status = validate_dict(data, office_key)
+            if not status == ok_str:
+                return status
+            name = data[name_key]
+            office_type = data[type_key]
+            office = OfficeCont(name=name, office_type=office_type)
+            return office.add_office()
+        except KeyError as e:
+            return field_missing_resp(office_key, fields, e.args[0])
+
+    elif request.method == get_method:
+        data = []
+        offices = Office().get_all()
+        for office in offices:
+            data.append(office.to_json())
+        return success(200, data)
+
+
+@office_v1.route('/offices/<int:id>', methods=[get_method])
+def get_office_ep(id):
+    """" get specific offce by id """
+    try:
+        if validate_id(office_key, id) == ok_str:
+            office = OfficeCont(Id=id)
+            if request.method == get_method:
+                return office.get_office()
+        return not_found_resp(office_key)
+    except Exception as e:
+        return runtime_error_resp(e)
