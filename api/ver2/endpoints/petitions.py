4b7d1f78bb5ea81d0ff3c55a4ef834bd5fd0dcf9
wainainad60 <muffwaindan@gmail.com>
2019-02-20 00:36:40 +0000

    [ft #164080997] added try catch for petitions endpoints

--- api/ver2/endpoints/petitions.py
+++ api/ver2/endpoints/petitions.py
@@ -6,6 +6,7 @@ from api.ver1.ballot.strings import createdOn_key, createdBy_key, body_key
 from api.ver1.utils import check_form_data, no_entry_resp, field_missing_resp, error, success
 from api.ver1.offices.strings import office_key
 from api.ver2.models.petitions import Petition
+from api.ver2.utils.utilities import system_unavailable
 
 
 petitions_bp = Blueprint('petitions_bp', __name__)
@@ -14,23 +15,26 @@ petitions_bp = Blueprint('petitions_bp', __name__)
 @petitions_bp.route('/petitions/', methods=[post_method])
 @jwt_required
 def petitions():
-    fields = [createdBy_key, createdOn_key, office_key, body_key, evidence_key]
-    data = check_form_data(petition_key, request, fields)
-    if data:
-        try:
-            petition = Petition(
-                created_by=data[createdBy_key],
-                created_on=data[createdOn_key],
-                office_id=data[office_key],
-                body=data[body_key],
-                evidence=data[evidence_key]
-            )
-            if petition.validate_petition():
-                petition.create()
-                return success(status_201, [petition.to_json()])
-            else:
-                return error(petition.message, petition.code)
-        except Exception as e:
-            return field_missing_resp(petition_key, fields, e.args[0])
-    else:
-        return no_entry_resp(petition_key, fields)
+    try:
+        fields = [createdBy_key, createdOn_key, office_key, body_key, evidence_key]
+        data = check_form_data(petition_key, request, fields)
+        if data:
+            try:
+                petition = Petition(
+                    created_by=data[createdBy_key],
+                    created_on=data[createdOn_key],
+                    office_id=data[office_key],
+                    body=data[body_key],
+                    evidence=data[evidence_key]
+                )
+                if petition.validate_petition():
+                    petition.create()
+                    return success(status_201, [petition.to_json()])
+                else:
+                    return error(petition.message, petition.code)
+            except Exception as e:
+                return field_missing_resp(petition_key, fields, e.args[0])
+        else:
+            return no_entry_resp(petition_key, fields)
+    except Exception as e:
+        return system_unavailable(e)
