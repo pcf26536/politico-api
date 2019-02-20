902d9b4c1a6f5b48499378fb23952b3fece6ee30
wainainad60 <muffwaindan@gmail.com>
2019-02-20 20:42:50 +0000

    updated results tests with user headers

--- api/tests/ver2/test_results.py
+++ api/tests/ver2/test_results.py
@@ -1,5 +1,5 @@
 from api.tests.ver2.test_base import TestBase
-from api.ver2.utils.strings import v2_url_prefix
+from api.ver2.utils.strings import v2_url_prefix, token_key, authorization_key
 from api.strings import status_key, data_key, error_key, status_404, status_200
 from api.ver1.offices.strings import office_key
 from api.ver2.utils.test_data.office_test_data import correct_office
@@ -7,6 +7,7 @@ from api.ver2.utils.test_data.party_test_data import correct_party
 from api.ver2.utils.test_data.signup_test_data import user_with_correct_signup_data
 from api.ver2.utils.test_data.register_test_data import correct_candidate_infor
 from api.ver2.utils.test_data.vote_test_data import correct_vote
+from api.ver2.utils.test_data.login_test_data import user_with_correct_credentials
 
 
 class TestResults(TestBase):
@@ -17,6 +18,13 @@ class TestResults(TestBase):
             v2_url_prefix + '/auth/signup',
             json=user_with_correct_signup_data
         )  # user
+        res = self.client.post(
+            '/api/v2/auth/login',
+            json=user_with_correct_credentials
+        )
+        self.user_access_token = res.get_json()[data_key][0][token_key]
+        self.user_headers = {authorization_key: 'Bearer {}'.format(self.user_access_token)}
+
         self.client.post(v2_url_prefix + '/parties', json=correct_party, headers=self.admin_headers)
         self.client.post(v2_url_prefix + '/offices', json=correct_office, headers=self.admin_headers)
         self.client.post(
@@ -26,7 +34,7 @@ class TestResults(TestBase):
         )
         self.client.post(
             v2_url_prefix + '/votes/',
-            json=correct_vote, headers=self.admin_headers
+            json=correct_vote, headers=self.user_headers
         )
 
     # clear all lists after tests
@@ -37,9 +45,9 @@ class TestResults(TestBase):
     def test_get_result(self):
         """ Tests get results success """
         res = self.client.get(
-            v2_url_prefix + '/office/1/result', headers=self.admin_headers)
+            v2_url_prefix + '/office/1/result', headers=self.user_headers)
         data = res.get_json()
-
+        
         self.assertEqual(data[status_key], status_200)
         self.assertEqual(data[data_key][0][office_key], "Women Representative")
         self.assertEqual(res.status_code, status_200)
@@ -47,7 +55,7 @@ class TestResults(TestBase):
     def test_get_results_office_id_not_found(self):
         """ Tests invalid office id """
         res = self.client.get(
-            v2_url_prefix + '/office/10000000/result', headers=self.admin_headers)
+            v2_url_prefix + '/office/10000000/result', headers=self.user_headers)
         data = res.get_json()
 
         self.assertEqual(data[status_key], status_404)
