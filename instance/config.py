c8ef39c3fdf49d7b511ae39af51286d766a2341f
wainainad60 <muffwaindan@gmail.com>
2019-02-22 10:40:58 +0300

    [start #164162338] add DB URL configs

--- instance/config.py
+++ instance/config.py
@@ -6,21 +6,19 @@ class Config(object):
     """Parent configuration class."""
     DEBUG = False
     SECRET = os.getenv('SECRET')
-    DATABASE_URL = "dbname='politico' host='127.0.0.1' " \
-                   "port='5432' user='postgres' password='kadanieet'"
 
 
 class DevelopmentConfig(Config):
     """Configurations for Development."""
     DEBUG = True
+    DATABASE_URL = os.getenv('DATABASE_URL')
 
 
 class TestingConfig(Config):
     """Configurations for Testing"""
     TESTING = True
     DEBUG = True
-    DATABASE_URL = "dbname='politico_test' host='127.0.0.1'" \
-                   " port='5432' user='postgres' password='kadanieet'"
+    DATABASE_URL = os.getenv('TEST_DATABASE_URL')
 
 
 class ProductionConfig(Config):
